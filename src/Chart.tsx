2412a840c1c33ea1ebb88930ae299a953fc9d7c8
F3542239 Gabriel Braga Zamproni Lima <g.braga@bb.com.br>
2022-09-16 10:18:14 -0300

    requisição com cors à API do Arthur

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,30 +1,22 @@
+import { useEffect, useState } from "react";
 import { Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 
+interface Data {
+  cHAVE: number,
+  dT_CRIACAO: number,
+  dT_PROCESSAMENTO: number,
+  dIFF: number
+}
 export default function Chart(){
-  const options = {
-    method: 'GET',
-    
-  };
-  
-  fetch('http://datagrid.apps.k8sdesbb110.desenv.bb.com.br/rest/v2/caches/kafka-debezium-metrics/?action=entries', {
-    mode: 'no-cors',
-    headers: {
-      // cookie: '2cadf896751dbbc86a6fee1e42a978ca=2307d718eaca7f411e3aa0460f208363',
-      "Authorization": "Digest developer JWdZBxalcMmfgXOS"
-    }
-  })
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error("asdasd"));
-  let date = new Date();
-  date.setHours(0, 0, 0, 0);
+  const [data, setData] = useState<Data[]>([])
   let value = 100;
   const generateData = () => {
     value = Math.round((Math.random() * 10 - 5) + value);
     return {
-      name: "asd",
-      a: value,
-      b: -value+1
+      cHAVE: 1,
+      dT_CRIACAO: value,
+      dT_PROCESSAMENTO: -value+1,
+      dIFF: value+2
     };
   }
 
@@ -35,15 +27,35 @@ export default function Chart(){
     }
     return data;
   }
-  const data = generateDatas(10);
-
+  useEffect(() => {
+    // let d: Data[] = generateDatas(10)
+    fetch('https://aqt-poc-metrics-generator-aqt-poc-listener-db.apps.k8sdesbb110.desenv.bb.com.br/main/entries')
+    .then(res => console.log(res))
+    setData(generateDatas(10))
+  }, [])
+  
+  // fetch('http://datagrid.apps.k8sdesbb110.desenv.bb.com.br/rest/v2/caches/kafka-debezium-metrics/?action=entries', {
+  //   mode: 'cors',
+  //   credentials: 'include',
+  //   headers: {
+  //     // "Access-Control-Allow-Origin": "*",
+  //     // cookie: '2cadf896751dbbc86a6fee1e42a978ca=2307d718eaca7f411e3aa0460f208363',
+  //     // "Authorization": "Digest developer JWdZBxalcMmfgXOS"
+  //     "Authorization": 'Digest username="developer", realm="default", nonce="AAAAEAAPb8a87+XIzqDEuuzVtsKxayCEWuMaSCHGb7LeLgrjLi55gDXMsII=", uri="/rest/v2/caches/kafka-debezium-metrics/%7B%22_type%22%3A%22string%22%2C%22_value%22%3A%22teste%22%7D", algorithm=MD5, response="1c5fb0cd2a2e57a51f64e0cca130f5e0", opaque="00000000000000000000000000000000", qop=auth, nc=0000000c, cnonce="07dce14f557e56c0'
+  //   }
+  // })
+  //   .then(result => console.log(result))
+  //   .catch(err => console.error(err));
+  
   return (
-    <LineChart width={500} height={300} data={data}>
-        <XAxis dataKey="name"/>
+    <>
+    <LineChart width={500} height={300} data={data} title="Tempo de reconhecimento pelo Cache">
+        <XAxis dataKey="cHAVE"/>
         <YAxis/>
         <Tooltip />
-        <Line type="monotone" dataKey="a" activeDot={{ r: 8 }} stroke="#8884d8" />
-        <Line type="monotone" dataKey="b" stroke="#82ca9d" />
+        <Line type="monotone" dataKey="dIFF" stroke="#82ca9d" name="millisec"/>
       </LineChart>
-  )
+      <button onClick={() => setData([...data, generateData()])} >Adicionar data</button>
+    </>
+    )
 }
